b598cb0d27f5305baf23e8518ee51066e07b823c
lafierro4 <lafierro4@miners.utep.edu>
2023-10-31 01:35:49 -0600

    Added Main Menu, Modified UIManager logic

--- User_Interface/MenuView.py
+++ User_Interface/MenuView.py
@@ -2,8 +2,57 @@
 # Manages the various menu and options within the game, including the main menu, settings, and in-game menu.
 # It allows players to navigate and make selections within the game. 
 
+import pygame
+from User_Interface.Button import Button
+pygame.init()
+
 # Display Choice 
 # Informs the GameBoardView about the user’s display setting such as size, brightness, or color blindness. 
 # Pre-Conditions: \@requires self.is_initialized() 
 # Post-Condition: \@ensures self.ui.display_updated_settings() 
 # Method Signature: def display_choice(self) -> None: 
+
+title_font = pygame.font.SysFont("consolas", 75)
+button_font =  pygame.font.SysFont("consolas", 50)
+
+def menu(SCREEN: pygame.Surface,FPS) -> int:
+    bg_image = pygame.transform.scale(pygame.image.load("assets\images\\title_bg.png"), (SCREEN.get_size()[0], SCREEN.get_size()[1]))
+    bg_image.set_alpha(128)
+    SCREEN.blit(bg_image,(0,0))
+    pygame.mixer.init()
+    pygame.mixer.music.load("assets\sounds\\title_bg_music.mp3")
+    pygame.mixer.music.play(loops= -1)
+
+
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        mouse_pos = pygame.mouse.get_pos()
+        menu_text = title_font.render("Cloneopoly", True, "black")
+        menu_text_rect = menu_text.get_rect(center = (SCREEN.get_size()[0]/2, SCREEN.get_size()[1]/5))
+      
+        play_button = Button(image = None, pos=(SCREEN.get_size()[0]/2, SCREEN.get_size()[1]/3), text_input= "Play", font= button_font , base_color="#39FF14", hover_color= "#0a18f3")
+        options_button = Button(image= None, pos=(SCREEN.get_size()[0]/2, SCREEN.get_size()[1]/3 + 100), text_input= "Options", font= button_font, base_color= "#39FF14", hover_color= "#0a18f3")
+        quit_button = Button(image= None, pos=(SCREEN.get_size()[0]/2, SCREEN.get_size()[1]/3 + 200), text_input= "Quit", font= button_font, base_color= "#39FF14", hover_color= "#0a18f3")
+        
+        SCREEN.blit(menu_text,menu_text_rect)
+      
+        for button in [play_button, options_button, quit_button]:
+            button.changeColor(mouse_pos)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.checkForInput(mouse_pos):
+                    pygame.mixer.music.stop()
+                    return 1
+                if options_button.checkForInput(mouse_pos):
+                    return 2
+                if quit_button.checkForInput(mouse_pos):
+                    return 3
+        pygame.display.update()
+        clock.tick(FPS)
+    return 0
+
--- User_Interface/UIManager.py
+++ User_Interface/UIManager.py
@@ -1,14 +1,20 @@
 # UIManager
 # Responsible for managing and coordination the game’s user interface. Interacts with other UI components for rendering and displaying the game information. 
 # This is where we will have all the pygame components, along with the other UI classes
+
+# TO DO: Agree on short cut names for imports
 import pygame
-from  User_Interface.GameboardView import GameboardView
-WIDTH, HEIGHT = 1280, 720
+from User_Interface.GameboardView import GameboardView
 from Game_Engine.Player import Player
+import User_Interface.MenuView as mv
 
 pygame.init()
+# Constants
+# FPS = the refresh rate of the in frames per second
+# SCREEN = the surface layer of the pygame window, the main layer
 FPS = 60
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+WIDTH, HEIGHT = 1280, 720
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Cloneopoly")
 
 # Initial Board Rendering 
@@ -29,39 +35,68 @@ pygame.display.set_caption("Cloneopoly")
 # Post-Condition: \@ensures self.ui.display_menu() 
 # Method Signature: def form_menu(self) -> None: 
 
-def initialize_player(WIN, pos_x, name, image):
-    # create a surface object, image is drawn on it.
-    token = pygame.image.load(image).convert_alpha()
-    # Scale the image
-    token = pygame.transform.scale(token, (40,40))
-    # Draw initial position of player on board
-    WIN.blit(token, (pos_x, 0))
-    player_one = Player(name, token)
-    return player_one
 
-def run():
+
+def main_loop():
     """
-        The Main Game Loop
+        The Main Game Loop, Checks for Inputs and Transitions to other Screens/Game Loops
     """
     run = True
     clock = pygame.time.Clock()
-    gameboard_view = GameboardView(WIN)
-    gameboard_view.setup_board()
-    
-    pos_x = 0
-    player_one = initialize_player(WIN,pos_x,"michel","assets/car.png")
-
+    return_status = main_menu()
     while run:
+        if return_status == 0 or return_status == 3:
+             run = False
+        if return_status == 1:
+            return_status = initialize_gameboard()
+        if return_status == 2:
+             pass
+        # We might need this event handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Moves player with each click
-                if event.button == 1: 
-                    pos_x = player_one.move_player(WIN,gameboard_view, pos_x)
+
         pygame.display.update()
         clock.tick(FPS)
 
     pygame.quit()
 
-run()
+def main_menu():
+    menu_choice = mv.menu(SCREEN,FPS)
+    return menu_choice
+
+# TO DO Move Functionality below to GameboardView
+def initialize_player(SCREEN, pos_x, name, image):
+    # create a surface object, image is drawn on it.
+    token = pygame.image.load(image).convert_alpha()
+    # Scale the image
+    token = pygame.transform.scale(token, (40,40))
+    # Draw initial position of player on board
+    SCREEN.blit(token, (pos_x, 0))
+    player_one = Player(name, token)
+    return player_one
+
+def initialize_gameboard():
+    # Initialize the board
+    pos_x = 0
+    player_one = initialize_player(SCREEN,pos_x,"michel","assets/images/car.png")
+    run = True
+    clock = pygame.time.Clock()
+    SCREEN.fill("black")
+    gameboard_view = GameboardView(SCREEN)
+    board_setup = gameboard_view.setup_board()
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Moves player with each click
+                if event.button == 1: 
+                    pos_x = player_one.move_player(SCREEN,gameboard_view, pos_x)
+
+        pygame.display.update()
+        clock.tick(FPS)
+    return 0
+
+main_loop()
