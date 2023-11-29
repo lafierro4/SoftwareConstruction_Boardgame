a66ff53908fb96055660a9fd73d3765d552bd760
lafierro4 <lafierro4@miners.utep.edu>
2023-11-28 23:38:31 -0700

    deleted gamboardmanager, moved to gamebaordview

--- User_Interface/GameboardView.py
+++ User_Interface/GameboardView.py
@@ -5,17 +5,57 @@ import pygame, os, textwrap, math
 
 FPS = 60
 WIDTH, HEIGHT = 1280, 720
-from Game_Engine.GameboardManager import Gameboard, Player, Property, Square
+from Game_Engine.GameboardManager import *
 from User_Interface.PlayerInfoView import *
 from User_Interface.util import *
 from User_Interface.MenuView import options_menu
 from AI.Strategy import Strategy
 class GameboardView:
     def __init__(self,screen:pygame.Surface):
-        self.gameboard = Gameboard()
         self.screen = screen
         self.space_size = screen.get_width() / 25.6
-        self.squares = self.gameboard._board
+        self._board = [
+            Square("Go","corner"),
+            Property("Mediterranean Meals", "property", "#a37759", 60, [2, 10, 30, 90, 160, 250]),
+            Square("Lice Tax", "tax"),
+            Property("Baltic Breezeway", "property", "#a37759", 60, [4, 20, 60, 180, 320, 450]),
+            Square("Income Tax", "tax"),
+            Property("Skipping Railroad", "property", "#000000", 200, [25, 50, 100, 200]),
+            Property("Oriental Oasis", "property", "#e8a541", 100, [6, 30, 90, 270, 400, 550]),
+            Square("Apple Tax", "tax"),
+            Property("Vermont Vacation", "property", "#e8a541", 100, [6, 30, 90, 270, 400, 550]),
+            Property("Connecticut Courtyard", "property", "#e8a541", 120, [8, 40, 100, 300, 450, 600]),
+            Square("Jail", "jail"),
+            Property("Sir Charles' Sanctuary", "property", "#a14685", 140, [10, 50, 150, 450, 625, 750]),
+            Property("Electric Company", "utility", "#a37759", 150),
+            Property("United Estates", "property", "#a14685", 140, [10, 50, 150, 450, 625, 750]),
+            Property("Virginia Vineyards", "property", "#a14685", 160, [12, 60, 180, 500, 700, 900]),
+            Property("Quarter Railroad", "property", "#000000", 200, [25, 50, 100, 200]),
+            Property("Saintly James' Square", "property", "#ef756d", 180, [14, 70, 200, 550, 750, 950]),
+            Square("Charity Tax", "tax"),
+            Property("Tunessee Avenue", "property", "#ef756d", 180, [14, 70, 200, 550, 750, 950]),
+            Property("Big Apple Avenue", "property", "#ef756d", 200, [16, 80, 220, 600, 800, 1000]),
+            Square("Free Parking", "corner"),
+            Property("Kentucky Fried Avenue", "property", "#ca6e47", 220, [18, 90, 250, 700, 875, 1050]),
+            Square("Bad Hair Tax", "tax"),
+            Property("Indy Car Avenue", "property", "#ca6e47", 220, [18, 90, 250, 700, 875, 1050]),
+            Property("Illusion Avenue", "property", "#ca6e47", 240, [20, 100, 300, 750, 925, 1100]),
+            Property("R. R. Railroad", "property", "#000000", 200, [25, 50, 100, 200]),
+            Property("Atlantic Adventure", "property", "#2277a2", 260, [22, 110, 330, 800, 975, 1150]),
+            Property("Ventilation Avenue", "property", "#2277a2", 260, [22, 110, 330, 800, 975, 1150]),
+            Property("Water Works", "utility", "#a37759", 150),
+            Property("Marvin's Magic Meadow", "property", "#2277a2", 280, [24, 120, 360, 850, 1025, 1200]),
+            Square("Go To Jail", "go_to_jail"),
+            Property("Pacific Playground", "property", "#55a95d", 300, [26, 130, 390, 900, 1100, 1275]),
+            Property("Northern Charm Avenue", "property", "#55a95d", 300, [26, 130, 390, 900, 1100, 1275]),
+            Square("Juice Tax", "tax"),
+            Property("Penny-sylvania Avenue", "property", "#55a95d", 320, [28, 150, 450, 1000, 1200, 1400]),
+            Property("Longline Railroad", "property", "#000000", 200, [25, 50, 100, 200]),
+            Square("Candy Tax", "tax"),
+            Property("Parking Place", "property", "#e34537", 350, [35, 175, 500, 1100, 1300, 1500]),
+            Square("Luxury Tax", "tax"),
+            Property("Bored Walk", "property", "#e34537", 400, [50, 200, 600, 1400, 1700, 2000]),
+        ]
         self.board_surface = pygame.Surface((self.screen.get_width(), self.screen.get_height()))
         self.border_width = 2
 
@@ -27,7 +67,7 @@ class GameboardView:
                 x = (col - 1) * self.space_size
                 y = (row - 1) * self.space_size
                 # space how to implement this
-                space = self.squares[index]
+                space = self._board[index]
                 if (row == 1 or row == 12):
                     if(col == 1 or col == 12):   
                         self.draw_rectangle(x, y, True, False, space.color)
@@ -162,7 +202,8 @@ class GameboardView:
         quit()
 
     def dice_is_being_rolled(self, players, dice_surfaces, current_player_index):
-        dice_rolls = self.gameboard.roll_dice()
+        random.seed()
+        dice_rolls =(random.randint(1, 6), random.randint(1, 6))
         steps = sum(dice_rolls)
         if players[current_player_index].in_jail():
             if all(roll == dice_rolls[0] for roll in dice_rolls):
@@ -178,7 +219,7 @@ class GameboardView:
 
 
     def display_action(self,player:Player, square_index):
-        current_space = self.squares[square_index]
+        current_space = self._board[square_index]
         if isinstance(current_space,Property):
             _display_property_action(self.screen,current_space,player)
         elif isinstance(current_space,Square):
